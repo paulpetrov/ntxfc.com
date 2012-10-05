15a6f9957b304856de4c1828572fe8592fa88019
Paul Petrov <airstrip@gmail.com>
2012-10-05 17:01:27 -0500

    Added files missed after SVN export.

---
+++ club/FlyingClub.BusinessLogic/MemberEqualityComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FlyingClub.Common;
+using FlyingClub.Data.Repository;
+using FlyingClub.Data.Repository.EntityFramework;
+using FlyingClub.Data.Model.Entities;
+
+namespace FlyingClub.BusinessLogic
+{
+    public class MemberEqualityComparer : IEqualityComparer<Member>
+    {
+        #region IEqualityComparer<Member> Members
+
+        public bool Equals(Member x, Member y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Member obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
